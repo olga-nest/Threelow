efcab49da332d0048daed6b0a4b5aac57c809842
olga-nest <olga.w.nesterova@gmail.com>
2017-10-04 18:22:39 -0700

    Initial Commit

---
+++ Threelow/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Threelow
+//
+//  Created by Olga on 10/4/17.
+//  Copyright © 2017 Olga. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
